9c3ed8f1904ffbb8e4466f86f0afe5361436f92f
Pedro Hos <pedro-hos@outlook.com>
2022-11-22 19:56:16 -0300

    aulas 1 a 4

---
+++ alura-course/hello/hello.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Olá, sr(a).", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O valor da variável comando é:", comandoLido)
+
+	return comandoLido
+}
