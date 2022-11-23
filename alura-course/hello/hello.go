928a19318be713c16086750294c0de005f8304bc
Pedro Hos <pedro-hos@outlook.com>
2022-11-23 18:45:05 -0300

    finalizando

--- alura-course/hello/hello.go
+++ alura-course/hello/hello.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
@@ -19,6 +28,7 @@ func main() {
 			iniciaMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -31,18 +41,56 @@ func main() {
 
 func iniciaMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br"}
+	sites := leArquivoSites()
 
-	for i := 0; i < len(sites); i++ {
-
-		resp, _ := http.Get(sites[i])
-
-		if resp.StatusCode == 200 {
-			fmt.Println("Site: ", sites[i], " carregado com sucesso!")
-		} else {
-			fmt.Println("Site: ", sites[i], " esta com problemas. Status code ", resp.StatusCode)
+	for i := 0; i < monitoramentos; i++ {
+		for _, site := range sites {
+			testaSite(site)
 		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
 
+func leArquivoSites() []string {
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site: ", site, " carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site: ", site, " esta com problemas. Status code ", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -64,4 +112,27 @@ func leComando() int {
 	fmt.Scan(&comandoLido)
 	fmt.Println("O valor da variável comando é:", comandoLido)
 	return comandoLido
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
 }
