f087ec9d76f29441cfe6de89024b9f1dc7933399
Pedro Hos <pedro-hos@outlook.com>
2022-11-23 10:56:28 -0300

    aulas até 5

--- alura-course/hello/hello.go
+++ alura-course/hello/hello.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciaMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func iniciaMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br"}
+
+	for i := 0; i < len(sites); i++ {
+
+		resp, _ := http.Get(sites[i])
+
+		if resp.StatusCode == 200 {
+			fmt.Println("Site: ", sites[i], " carregado com sucesso!")
+		} else {
+			fmt.Println("Site: ", sites[i], " esta com problemas. Status code ", resp.StatusCode)
+		}
+
 	}
 }
 
@@ -42,6 +63,5 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O valor da variável comando é:", comandoLido)
-
 	return comandoLido
 }
